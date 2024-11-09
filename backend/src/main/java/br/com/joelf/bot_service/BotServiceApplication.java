baf57eb1991b4ef34e990dc45b13a277df3b8ad3
joelfilho <joelfilho@ciandt.com>
2024-11-09 10:58:59 -0300

    chore(backend): init project

---
+++ backend/src/main/java/br/com/joelf/bot_service/BotServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.bot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotServiceApplication.class, args);
+	}
+
+}
