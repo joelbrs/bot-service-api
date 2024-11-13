3a6a55029cb8cde0ee9a14a8e7cff375d1e03d28
joelfilho <joelfilho@ciandt.com>
2024-11-13 12:20:48 -0300

    feat(backend): add exception handlers to authentication exceptions

--- src/main/java/br/com/joelf/bot_service/infraestructure/entrypoint/controllers/exceptions/ExceptionHandlerImpl.java
+++ src/main/java/br/com/joelf/bot_service/infraestructure/entrypoint/controllers/exceptions/ExceptionHandlerImpl.java
@@ -1,11 +1,15 @@
 package br.com.joelf.bot_service.infraestructure.entrypoint.controllers.exceptions;
 
 import br.com.joelf.bot_service.domain.usecase.exceptions.*;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 
 @ControllerAdvice
@@ -58,6 +62,57 @@ public class ExceptionHandlerImpl {
             HttpServletRequest request, FindTemplateByIdUseCaseException e
     ) {
         return mapResponse(request, e);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> badCredentialsException(
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        String msg = "Credenciais inválidas! Usuário e/ou senha incorretos.";
+
+        return ResponseEntity.status(status).body(
+                ExceptionResponse.builder()
+                        .timestamp(Instant.now())
+                        .status(status.value())
+                        .error(msg)
+                        .path(request.getRequestURI())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ExceptionResponse> accessDeniedException(
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        String msg = "Acesso negado! Você não tem permissão para acessar este recurso.";
+
+        return ResponseEntity.status(status).body(
+                ExceptionResponse.builder()
+                        .timestamp(Instant.now())
+                        .status(status.value())
+                        .error(msg)
+                        .path(request.getRequestURI())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ExceptionResponse> expiredJwtException(
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        String msg = "Token expirado! Faça login novamente para obter um novo token.";
+
+        return ResponseEntity.status(status).body(
+                ExceptionResponse.builder()
+                        .timestamp(Instant.now())
+                        .status(status.value())
+                        .error(msg)
+                        .path(request.getRequestURI())
+                        .build()
+        );
     }
 
     private ResponseEntity<ExceptionResponse> mapResponse(
