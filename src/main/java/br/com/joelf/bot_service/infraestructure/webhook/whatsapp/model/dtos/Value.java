21593d7540b8602083e76372c938e8546df0dc01
joelfilho <joelfilho@ciandt.com>
2024-11-18 13:45:18 -0300

    feat(webhook-v2): add provisional toString in entrypoint dto

--- src/main/java/br/com/joelf/bot_service/infraestructure/webhook/whatsapp/model/dtos/Value.java
+++ src/main/java/br/com/joelf/bot_service/infraestructure/webhook/whatsapp/model/dtos/Value.java
@@ -3,9 +3,11 @@ package br.com.joelf.bot_service.infraestructure.webhook.whatsapp.model.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Value {
 
     @JsonProperty("messaging_product")
