f99366834425137c0668cb3d91778ab0aea73e86
joelfilho <joelfilho@ciandt.com>
2024-11-18 13:13:15 -0300

    feat(webhook-v2): add logger when receive a msg

--- src/main/java/br/com/joelf/bot_service/infraestructure/webhook/whatsapp/entrypoint/MetaWebhook.java
+++ src/main/java/br/com/joelf/bot_service/infraestructure/webhook/whatsapp/entrypoint/MetaWebhook.java
@@ -4,6 +4,8 @@ import br.com.joelf.bot_service.domain.usecase.SendMessageUseCase;
 import br.com.joelf.bot_service.infraestructure.webhook.whatsapp.model.dtos.Entrypoint;
 import br.com.joelf.bot_service.infraestructure.webhook.whatsapp.model.dtos.Message;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/webhook")
 public class MetaWebhook {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetaWebhook.class);
     private final SendMessageUseCase sendMessageUseCase;
 
     @GetMapping("/whatsapp")
@@ -21,8 +24,9 @@ public class MetaWebhook {
 
     @PostMapping("/whatsapp")
     public ResponseEntity<Void> mountMessage(@RequestBody Entrypoint body) {
+        LOGGER.info("Received message: {}", body);
         Message message = body.getEntry().getFirst().getChanges().getFirst().getMessages().getFirst();
         sendMessageUseCase.execute(message.getText().getBody(), message.getFrom());
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 }
